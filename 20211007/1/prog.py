c7de44b8f6a4c6c2222e3f0f6dede651d5bc5fbc
stamplevskiyd <stamplevskiyd@gmail.com>
2021-10-08 10:25:45 +0300

    modified first task with tests

--- 20211007/1/prog.py
+++ 20211007/1/prog.py
@@ -1,6 +1,5 @@
 def Pareto(*data):
     res = ()
-    print(data)
     for i in range(len(data)):
         for j in range(len(data)):
             if i != j and data[i][0] <= data[j][0] and data[i][1] <= data[j][1]\
@@ -10,3 +9,5 @@ def Pareto(*data):
             res = res + (data[i],)
     return res        
 
+# для тестов
+print(Pareto(*(eval(input()))))
