f71846e077f7f8a2f46c9af5e40ff6cb025777f9
stamplevskiyd <stamplevskiyd@gmail.com>
2021-10-20 13:41:02 +0300

    Added first task with tests

--- 20211014/1/prog.py
+++ 20211014/1/prog.py
@@ -1,30 +1,19 @@
 from fractions import Fraction
-data_list = input().split(', ')
+
+def Counter(s, w, arguments):  # никак не зависит от типа аргументов
+    pol1 = pol2 = s - s # задаем 0 нужного типа (не очень надо, для наглядности)
+    deg1 = int(arguments[0])
+    deg2 = int(arguments[deg1 + 2])
+    for i in range(deg1 + 1):
+        pol1 += arguments[i + 1] * (s ** (deg1 - i))
+    for i in range(deg2 + 1):
+        pol2 += arguments[i + deg1 + 3] * (s ** (deg2 - i))
+    return pol1/pol2 == w if pol2 else False
+
+data_list = input().strip().split(',')
+for i in range(len(data_list)):  # перевод строк в числа
+    data_list[i] = Fraction(data_list[i])
 s = data_list[0]
 w = data_list[1]
-deg1 = int(data_list[2])
-deg2 = int(data_list[deg1 + 3])
+print(Counter(s, w, data_list[2:]))
 
-pol1 = Fraction("0.0")
-pol2 = Fraction("0.0")
-#print("deg1 = ", deg1)
-#print("deg2 = ", deg2)
-for i in range(deg1 + 1):
-    pol1 += Fraction(data_list[i + 3]) * (Fraction(s) ** (deg1 - i))  # +3, так как 3 лишних символа: w, s, степень первого
-#    print(Fraction(data_list[i + 3]))
-#print("_"*10)    
-for i in range(deg2 + 1):
-    pol2 += Fraction(data_list[i + deg1 + 5]) * (Fraction(s) ** (deg2 - i))
-#    print(Fraction(data_list[i + deg1 + 5]) )
-#print(pol1)
-#print(pol2)
-#print(w)
-#print(pol1/pol2)
-#print(pol1/pol2 == w)
-
-def Counter(s, w, *arguments):
-    pol1 = s - s # задаем 0 нужного типа
-    pol2 = s - s
-    for i in range(deg1 + 1):
-        pol1 += arguments[i] * (s ** (deg1 - i))
-    
