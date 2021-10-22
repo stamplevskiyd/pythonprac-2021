ddad54f9ccbf4c6f2ce0f0e2c20c408d9f89c4af
stamplevskiyd <stamplevskiyd@gmail.com>
2021-10-22 17:33:12 +0300

    Added second task wih tests

--- 20211021/2/prog.py
+++ 20211021/2/prog.py
@@ -2,11 +2,13 @@ import math
 
 functions = {}
 while (data := input().split())[0] != "quit":
-    print(functions)
-    print(data)
     if data[0][0] == ':':
-        functions[data[0][1:]] = (data[0][1:], data[1], data[2])
+        for i in range(3, len(data)):  # чтобы можно было писать выражения с пробелами
+           data[2] += ' ' + data[i]
+        functions[data[0][1:]] = {'new_name': data[0][1:], 'arg': data[1], 'old_name': data[2]}
     else:
-        print(functions[data[0]], {functions[data[0]][0]: eval(data[1])})
-        print(eval(functions[data[0]], math.__dict__, {functions[data[0]][1]: eval(data[1])}))
-print(functions)        
+        for i in range(2, len(data)):  # чтобы можно было писать выражения с пробелами
+           data[1] += ' ' + data[i]
+        print(eval(functions[data[0]]['old_name'], {functions[data[0]]['arg']: eval(data[1])}, math.__dict__))
+print(len(functions) + 1)
+
