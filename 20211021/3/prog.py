b877e31b67b0e82924afec5f79284c97ff5a7a71
stamplevskiyd <stamplevskiyd@gmail.com>
2021-10-27 16:38:16 +0300

    Added 3rd task

---
+++ 20211021/3/prog.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+W = int(input())
+signs = ' ~!@#$%^&*()-_=+\|/]}[{;:\'\",<.>/?\t'  # можно и еще добавить, но вроде хватит
+data = []
+while line := input():
+    for sign in signs:
+        line = line.replace(sign, '@')
+    for word in line.split('@'):
+        if word and len(word) == W:
+            data.append(word)
+c = Counter(data)
+if c:
+    max_count = c.most_common()[0][1]
+    for item in sorted([word for word in c if c[word] == max_count]):
+        print(item, end=' ')
+
