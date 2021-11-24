082396ab7addcf3df26e0d3e59a887b83a873c0d
stamplevskiyd <stamplevskiyd@gmail.com>
2021-11-24 22:19:07 +0300

    Added 3rd task(without tests) and improved first

--- 20211118/1/prog.py
+++ 20211118/1/prog.py
@@ -1,15 +1,43 @@
-def objcount(cls):
-    class New(cls):
-        counter = 0
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            New.counter += 1
+import string
 
-        def __del__(self):
-            if hasattr(super(), "__del__"):
-                super().__del__()
-            New.counter -= 1
-    return New
+class Empty:
+    pass
+
+class Alpha:
+    __slots__ = list(string.ascii_lowercase)
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def __str__(self):
+        data = ''
+        for letter in string.ascii_lowercase:
+            if hasattr(self, letter):
+                data += letter + ': ' + str(getattr(self, letter, None)) + ', '
+        return data[:-2]
+
+class AlphaQ(Empty):
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            if key in string.ascii_lowercase:
+                setattr(self, key, value)
+            else:
+                raise AttributeError
+
+    def __str__(self):  # same as in Alpha
+        data = ''
+        for letter in string.ascii_lowercase:
+            if hasattr(self, letter):
+                data += letter + ': ' + str(getattr(self, letter, None)) + ', '
+        return data[:-2]
+
+    def __setattr__(self, key, value):
+        if key in string.ascii_lowercase:
+            super().__setattr__(key, value)
+        else:
+            raise AttributeError
+
 
 import sys
 exec(sys.stdin.read())
--- 20211118/3/prog.py
+++ 20211118/3/prog.py
@@ -1,8 +1,22 @@
 import string
-print(string.ascii_lowercase)
 
 class Alpha:
     __slots__ = list(string.ascii_lowercase)
 
     def __init__(self, **kwargs):
-    #использовать для этого setattr. Или нет
+        for i, j in kwargs.items():
+            setattr(self, i, j)
+
+    def __str__(self):
+        data = ''
+        for letter in string.ascii_lowercase:
+            if hasattr(self, letter):
+                data += letter + ': ' + str(getattr(self, letter, None)) + ', '
+        return data[:-2]
+
+class AlphaQ:
+    #далее не успел. Alpha - рабочий
+
+alp = Alpha(c=10, z=2, a=42)
+alp.e = 123
+print(alp)
