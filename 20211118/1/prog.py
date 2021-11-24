798fa5b57747a4c44f33e19b2b663f8c53c73afb
stamplevskiyd <stamplevskiyd@gmail.com>
2021-11-24 22:26:50 +0300

    Previous was old version of 1st task

--- 20211118/1/prog.py
+++ 20211118/1/prog.py
@@ -2,7 +2,10 @@ def objcount(cls):
     class New(cls):
         counter = 0
         def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
+            try:
+                super().__init__(*args, **kwargs)
+            except TypeError:
+                super().__init__()    
             New.counter += 1
 
         def __del__(self):
