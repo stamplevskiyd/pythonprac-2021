a254a90a99b1887f4fd292d81f435423f77c73c9
stamplevskiyd <stamplevskiyd@gmail.com>
2021-12-08 16:08:08 +0300

    Была ошибка: дробь сдвигалась не в ту сторону. Поправлено

--- 20211014/3/prog.py
+++ 20211014/3/prog.py
@@ -10,11 +10,11 @@ while (line := input()) != ('#'*width):
             gas_count += 1
         elif symbol == '~':
             liquid_count += 1
-height += 1  # нижнюю грань ящика не посчитали            
-# округление в большую сторону без дополнительных импортов         
+height += 1  # нижнюю грань ящика не посчитали
+# округление в большую сторону без дополнительных импортов
 liquid_lines = round(liquid_count / (height - 2))
 if liquid_lines * (height - 2) < liquid_count:
-    liquid_lines += 1    
+    liquid_lines += 1
 gas_lines = width - 2 - liquid_lines
 
 
@@ -25,7 +25,10 @@ for i in range(liquid_lines):
     print('#' + '~' * (height - 2) + '#')
 print('#' * height)
 
-# статичтика выводится по изначальным данным
+# статистика выводится по изначальным данным
 max_len = max(gas_count, liquid_count)
-print('{:<21}'.format('.'* round(20*gas_count/max_len)), gas_count, '/', gas_count + liquid_count, sep='')
-print('{:<21}'.format('~'* round(20*liquid_count/max_len)), liquid_count, '/', gas_count + liquid_count, sep='')
+shift = max(len(str(liquid_count)), len(str(gas_count)))
+gas_line = '.' * round(20 * gas_count / max_len)
+liquid_line = '~' * round(20 * liquid_count / max_len)
+print(f'{gas_line:<20} {gas_count:>{shift}}/{gas_count + liquid_count}')
+print(f'{liquid_line:<20} {liquid_count:>{shift}}/{gas_count + liquid_count}')
