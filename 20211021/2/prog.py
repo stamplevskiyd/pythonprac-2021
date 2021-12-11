c8b2bb68d2766171bb51c27a9aa00c985ccf2f38
stamplevskiyd <stamplevskiyd@gmail.com>
2021-12-11 09:44:36 +0300

    Недопонял, что именно было нужно. Исправил вывод

--- 20211021/2/prog.py
+++ 20211021/2/prog.py
@@ -3,11 +3,12 @@ import math
 functions = {}
 counter = 1
 while (data := input().split())[0] != "quit":
+    counter += 1
     if data[0][0] == ':':
         functions[data[0][1:]] = data[1:]
     else:
         print(eval(functions[data[0]][-1], math.__dict__,
                    {functions[data[0]][i]: eval(data[1 + i]) for i in range(len(data) - 1)}))
-    counter += 1
-print("{}:{}".format(counter, len(functions) + 1))
+
+print(data[1].format(len(functions) + 1, counter))
 
