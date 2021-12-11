b5874af0543d9142086d0f4204f0a97dbebfd138
stamplevskiyd <stamplevskiyd@gmail.com>
2021-12-11 10:46:59 +0300

    Added first task

--- 20211202/1/prog.py
+++ 20211202/1/prog.py
@@ -1,31 +1,17 @@
 def decorator(fun):
     def new(*args, **kwargs):
-        print(fun.__name__, args, kwargs)
+        print("{}: {}, {}".format(fun.__name__, args[1:], kwargs))
         return fun(*args, **kwargs)
     return new
 
 
-def dump(type):
-    def __init__(self, name, parents, ns):
-        for elem in ns:
-            
-    
-
-class ctype(type):
-
-    def __call__(self, *args, **kwargs):
-        print("call", self, args, kwargs)
-        return super().__call__(*args, **kwargs)
-
+class dump(type):
     def __new__(cls, name, parents, ns):
-        print("new", cls, name, parents, ns)
+        for elem in ns.items():
+            if callable(elem[1]):
+                ns[elem[0]] = decorator(elem[1])
         return super().__new__(cls, name, parents, ns)
 
-    def __init__(self, name, parents, ns):
-        print("init", self, parents, ns)
-        return super().__init__(name, parents, ns)
 
-class C(int, metaclass=ctype):
-     field = 42
-
-c = C("100500", base=16)
+import sys
+exec(sys.stdin.read())
