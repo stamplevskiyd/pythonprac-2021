9c48e7f35744700c72193c8fd5c0beb7e241e903
stamplevskiyd <stamplevskiyd@gmail.com>
2021-12-19 19:31:46 +0300

    Поправлены тесты

--- 20211021/2/prog.py
+++ 20211021/2/prog.py
@@ -1,14 +1,12 @@
 import math
 
 functions = {}
-counter = 1
 while (data := input().split())[0] != "quit":
-    counter += 1
     if data[0][0] == ':':
         functions[data[0][1:]] = data[1:]
     else:
         print(eval(functions[data[0]][-1], math.__dict__,
                    {functions[data[0]][i]: eval(data[1 + i]) for i in range(len(data) - 1)}))
 
-print('{}:{}'.format(len(functions) + 1, counter))
+print(len(functions) + 1)
 
