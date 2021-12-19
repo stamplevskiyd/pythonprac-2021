40dab1dfffda44406c999251ed3c1f87e29769cb
stamplevskiyd <stamplevskiyd@gmail.com>
2021-12-19 20:03:21 +0300

    По условию должен возвращаться кортеж отрезков. Для случая с 1 отрезком это работало не так

--- 20211007/1/prog.py
+++ 20211007/1/prog.py
@@ -1,7 +1,7 @@
 def Pareto(*data):
     res = ()
     if len(data) == 2:  # на случай если пара одна
-        return data
+        return (data, )
     for i in range(len(data)):
         for j in range(len(data)):
             if i != j and data[i][0] <= data[j][0] and data[i][1] <= data[j][1]\
