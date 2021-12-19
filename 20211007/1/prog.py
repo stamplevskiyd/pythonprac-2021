8a15704138dfa99374e2a855ec052e69f9d1861c
stamplevskiyd <stamplevskiyd@gmail.com>
2021-12-19 12:20:54 +0300

    Добавлена работа с одной парой чисел как с параметром

--- 20211007/1/prog.py
+++ 20211007/1/prog.py
@@ -1,5 +1,7 @@
 def Pareto(*data):
     res = ()
+    if len(data) == 2:  # на случай если пара одна
+        return data
     for i in range(len(data)):
         for j in range(len(data)):
             if i != j and data[i][0] <= data[j][0] and data[i][1] <= data[j][1]\
@@ -7,8 +9,6 @@ def Pareto(*data):
                 break
         else:
             res = res + (data[i],)
-    return res        
+    return res
 
-# для тестов
-import sys
-exec(sys.stdin.read())
+print(Pareto(*eval(input())))
