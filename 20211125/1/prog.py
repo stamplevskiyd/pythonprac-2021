30eab91f51d65013b98be44993d13c6878e339ec
stamplevskiyd <stamplevskiyd@gmail.com>
2021-12-19 12:38:14 +0300

    Исправлена задача

--- 20211125/1/prog.py
+++ 20211125/1/prog.py
@@ -1,9 +1,7 @@
 import sys
-from math import ceil
 s = sys.stdin.buffer.read()
 count = s[0]
-size = ceil((len(s) - 1) / count)
 beginning = s[0:1]
 s = s[1:]
-lines_list = sorted((s[i * size : (i + 1) * size] for i in range(len(s) // size)))
+lines_list = sorted((s[i * len(s) // count : (i + 1) * len(s) // count] for i in range(0, count, 1)))
 sys.stdout.buffer.write(beginning + b''.join(lines_list))
