ee6defee8763f9263d4f3ab8316b4c00428650b7
stamplevskiyd <stamplevskiyd@gmail.com>
2021-12-19 18:22:42 +0300

    Исправлена задача

--- 20210930/1/prog.py
+++ 20210930/1/prog.py
@@ -1,6 +1,6 @@
 data = list(eval(input()))
 for i in range(len(data) - 1):
-    for j in range(i,len(data)):
-        if (data[i] ** 2) % 100 > (data[j] ** 2) % 100:
-            data[i], data[j] = data[j], data[i]
+    for j in range(len(data) - i - 1):
+        if (data[j] ** 2) % 100 > (data[j + 1] ** 2) % 100:
+            data[j], data[j + 1] = data[j + 1], data[j]
 print(data)            
