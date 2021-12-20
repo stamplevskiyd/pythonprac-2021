54acfd517cb660c097fc21f6c0fecb8ce1d833b5
stamplevskiyd <stamplevskiyd@gmail.com>
2021-12-20 10:47:54 +0300

    Пытаюсь понять условия и подстроить вывод под них

--- 20211021/2/prog.py
+++ 20211021/2/prog.py
@@ -1,12 +1,14 @@
 import math
 
 functions = {}
+counter = 1
 while (data := input().split())[0] != "quit":
+    counter += 1
     if data[0][0] == ':':
         functions[data[0][1:]] = data[1:]
     else:
         print(eval(functions[data[0]][-1], math.__dict__,
                    {functions[data[0]][i]: eval(data[1 + i]) for i in range(len(data) - 1)}))
 
-print(len(functions) + 1)
+print(eval(data[1]).format(len(functions) + 1, counter))
 
