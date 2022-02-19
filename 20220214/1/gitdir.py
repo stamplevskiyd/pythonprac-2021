2681a539f0af27ace7161794588e944ab46a0a57
stamplevskiyd <stamplevskiyd@gmail.com>
2022-02-19 11:07:30 +0300

    V2. Added printing last commit in chosen tree
    Prints last commit (tree, parent, author... and commit text).
    Launch format: python3 gitdir.py tree_name

--- 20220214/1/gitdir.py
+++ 20220214/1/gitdir.py
@@ -1,6 +1,13 @@
-from os import listdir
+import os
+import sys
+import zlib
 
-branch_list = '.git/refs/heads'
-for branch_name in listdir(branch_list):
-    print(branch_name)
-    
+branch_name = sys.argv[1]
+branch_file = '.git/refs/heads/' + branch_name
+with open(branch_file) as file:
+    branch_code = file.read()
+commit_path = '.git/objects/'+ branch_code[:2] + '/'+ branch_code[2:]
+with open(commit_path[:-1], 'rb') as objfile:
+    fullobj = zlib.decompress(objfile.read())
+    header, _, body = fullobj.partition(b'\x00')
+    print(body.decode())
