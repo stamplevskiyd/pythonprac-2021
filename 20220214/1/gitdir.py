da61e0847b74e29d7f43d99ada8eee482d0554da
stamplevskiyd <stamplevskiyd@gmail.com>
2022-02-20 13:52:09 +0300

    V4. Added tree-looking repository output
    Input is the same: python3 gitdir.py <branch_name>
    Output format:
    
    File
    Another_file.txt
    Folder
    --- file
    --- subdir
    --- --- file2
    file3

--- 20220214/1/gitdir.py
+++ 20220214/1/gitdir.py
@@ -1,6 +1,22 @@
 import sys
 import zlib
 
+
+def printer(name, id, height):
+    type, filename = name.split()[0], name.split()[1]
+    print('--- ' * height + str(filename)[2:-1] + ', [' + id + ']')
+    if type == b'40000':  # папка
+        tree_path = tree_path = '.git/objects/' + id[:2] + '/' + id[2:]
+        with open(tree_path, 'rb') as tree_file:
+            treeobj = zlib.decompress(tree_file.read())
+            header, _, body = treeobj.partition(b'\x00')
+            while body:
+                treehdr, _, tail = body.partition(b'\x00')
+                gitid, body = tail[:20], tail[20:]
+                printer(treehdr, gitid.hex(), height + 1)
+        
+
+
 branch_name = sys.argv[1]
 branch_file = '.git/refs/heads/' + branch_name
 with open(branch_file) as file:
@@ -19,5 +35,4 @@ with open(tree_path, 'rb') as tree_file:
     while body:
         treehdr, _, tail = body.partition(b'\x00')
         gitid, body = tail[:20], tail[20:]
-        print(f'{treehdr}, {gitid.hex()}')
-
+        printer(treehdr, gitid.hex(), 0)
