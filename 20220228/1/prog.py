3a51ee90bc0e6c59d6b2014dfe9ee3b3dd89912a
stamplevskiyd <stamplevskiyd@gmail.com>
2022-03-03 15:02:13 +0300

    V2.3

--- 20220228/1/prog.py
+++ 20220228/1/prog.py
@@ -1,7 +1,12 @@
 import textdistance as td
 
 def dist(s1, s2, s3):
-    return td.levenshtein(s1, s2)
+    if s3 == "L":
+        return td.levenshtein(s1, s2)
+    elif s3 == "D":
+        return td.damerau_levenshtein(s1, s2)
+    else:
+        return -1
 
 s1 = input()
 s2 = input()
