da3e683832656863cdef194cce4264722dd0137d
stamplevskiyd <stamplevskiyd@gmail.com>
2022-04-23 11:05:46 +0300

    Fixed first task according to updated task

--- 20220418/1/square_solver.py
+++ 20220418/1/square_solver.py
@@ -6,6 +6,6 @@ def solveSquare(a, b, c):
     if dis < 0:
         return None
     elif dis == 0:
-        return -b / (2 * a)
+        return -b / (2 * a), -b / (2 * a)
     else:
         return (-b - sqrt(dis)) / (2 * a), (-b + sqrt(dis)) / (2 * a)
--- 20220418/1/test_square_solver.py
+++ 20220418/1/test_square_solver.py
@@ -8,7 +8,7 @@ class TestSolver(unittest.TestCase):
         self.assertEqual(solveSquare(a=1, b=-3, c=2), (1.0, 2.0))
 
     def test_01(self):
-        self.assertEqual(solveSquare(a=1, b=-4, c=4), 2.0)
+        self.assertEqual(solveSquare(a=1, b=-4, c=4), (2.0, 2.0))
 
     def test_02(self):
         self.assertEqual(solveSquare(a=1, b=0, c=1), None)
